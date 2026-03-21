652aac53c0bc41f7158f4fb5cb8fd00f1acfd7ba
Yonghwan Yoo <yyh971001@gmail.com>
2026-03-21 15:33:52 +0900

    mini-router interface

--- top/uvm_tb/interface.sv
+++ top/uvm_tb/interface.sv
@@ -1,10 +1,19 @@
-interface sync_fifo_if #(parameter D_WIDTH=4)(input clk, input rst_n);
+interface mini_router_if #(parameter D_WIDTH=8)(logic clk, logic rst_n);
 
-	logic wr_en;
-	logic rd_en;
-	logic [D_WIDTH-1:0] din;
-	logic [D_WIDTH-1:0] dout; 
-	logic full;
-	logic empty;
+	logic  [D_WIDTH-1:0]    s_tdata_0;
+    logic  [D_WIDTH-1:0]    s_tdata_1;
+    logic  [D_WIDTH-1:0]    s_tdata_2;
+    logic  [D_WIDTH-1:0]    s_tdata_3;
+    logic  [3:0]            s_tvalid;
+    logic  [3:0]            s_tlast;
+    logic  [3:0]            s_tdest;
+    logic  [3:0]            s_tready;
+
+    // Master side (mini_router -> downstream)
+    logic [D_WIDTH-1:0]    m_tdata_0;
+    logic [D_WIDTH-1:0]    m_tdata_1;
+    logic [1:0]            m_tvalid;
+    logic [1:0]            m_tlast;
+    logic [1:0]           m_tready;
   
 endinterface
